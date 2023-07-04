612ff3b8a0eb2c75d0e79514050c073d2ad3b387
tobe masa <t.masa.works@gmail.com>
2023-07-04 20:26:38 +0900

    じゃんけん再提出

--- ruby_exam/janken.rb
+++ ruby_exam/janken.rb
@@ -6,23 +6,16 @@ class Player
     puts "0:グー, 1:チョキ, 2:パー"
     # 変数「input_hand」にプレイヤーの入力値を代入します。
     # ヒント：getsメソッドについて調べてみましょう。
-    input_hand = gets.to_i
+    input_hand = gets.chomp
     # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
-    while input_hand != 0 || input_hand != 1 || input_hand != 2
-      # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
-      if input_hand == 0 || input_hand == 1 || input_hand == 2
-      # ヒント：include?メソッドについて調べてみましょう。
-        # 「input_hand」をそのまま返す。
-        return input_hand
-        # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-       else
-        # プレイヤーに「0〜2」を入力させる文章を表示させる。
+    while !["0", "1", "2"].include?(input_hand)
+
         puts "0〜2の数字を入力してください。"
         puts "0:グー, 1:チョキ, 2:パー"
         # 変数「input_hand」にプレイヤーの入力値を代入します。
-        input_hand = gets.to_i
-       end
+        input_hand = gets.chomp
     end
+    return input_hand.to_i
   end
 end
 # 相手が「0~2」の値をランダムに生成するロジックを書きます。
